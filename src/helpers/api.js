93a5f04e39cb805f97c7b088cac6359f82de0dc0
engWafula <wafulaallan5@gmail.com>
2025-07-03 11:20:22 +0300

    adding fixes for api.js file to point to .env variables

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
-    // baseURL: process.env.NODE_ENV === 'production' 
-    //     ? process.env.REACT_APP_API_URL_PROD 
-    //     : process.env.REACT_APP_API_URL_DEV
+    //baseURL: 'http://localhost:5000/api'
+    baseURL: process.env.NODE_ENV === 'production' 
+        ? process.env.REACT_APP_API_URL_PROD 
+        : process.env.REACT_APP_API_URL_DEV
 });
 
 export default API;
