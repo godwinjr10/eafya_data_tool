2c8cfa514e477e6fc8f779a4b24daad0ee84816e
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2025-07-03 12:47:25 +0300

    fix .env file

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
-    // baseURL: process.env.NODE_ENV === 'production' 
-    //     ? process.env.REACT_APP_API_URL_PROD 
-    //     : process.env.REACT_APP_API_URL_DEV
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV
 });
 
 export default API;
