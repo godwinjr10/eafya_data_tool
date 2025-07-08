35342425594ba9ab032671e016587f04868452bf
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2025-07-08 11:27:18 +0300

    fix mapping

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL_PROD  : process.env.REACT_APP_API_URL_DEV
 });
 
 export default API;
