70fd664e79e59063724c75e6003f17a8276fe9b5
Abigabi Deus <deusabigaba1234@gmail.com>
2025-08-30 10:13:54 +0300

    included disease_id column to materialized views admission_death

--- backend/sql/materializedviews/108_06_admission_death.sql
+++ backend/sql/materializedviews/108_06_admission_death.sql
@@ -1,7 +1,8 @@
---CREATE MATERIALIZED VIEW reporting."108_admission_death" AS
+CREATE MATERIALIZED VIEW reporting."108_admission_death" AS
 SELECT
     TO_CHAR("admission_date", 'YYYYMM') AS report_month,
     "diagnosis",
+    "disease_id",
 SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) <= 4 AND LOWER("gender") = 'male' THEN 1 ELSE 0 END) AS "0-4years Male Cases",
     SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) <= 4 AND LOWER("gender") = 'female' THEN 1 ELSE 0 END) AS "0-4years female Cases",
     SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) >= 5 AND LOWER("gender") = 'male' THEN 1 ELSE 0 END) AS "5years+ Male Cases",
@@ -11,7 +12,7 @@ SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) <= 4 AND LO
     SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) >= 5 AND LOWER("gender") = 'male' AND "death_date" IS NOT NULL THEN 1 ELSE 0 END) AS "5years+ Male Deaths",
     SUM(CASE WHEN DATE_PART('year', age("admission_date", "birth_date")) >= 5 AND LOWER("gender") = 'female' AND "death_date" IS NOT NULL THEN 1 ELSE 0 END) AS "5years+ female Deaths"
 FROM reporting."108_inpatient"
-GROUP BY TO_CHAR("admission_date", 'YYYYMM'), "diagnosis"
+GROUP BY TO_CHAR("admission_date", 'YYYYMM'), "diagnosis" , "disease_id"
 ORDER BY report_month DESC, "diagnosis";
 
 
--- backend/sql/tablescripts/004_reporting_tables.sql
+++ backend/sql/tablescripts/004_reporting_tables.sql
@@ -439,13 +439,14 @@ CREATE TABLE IF NOT EXISTS reporting."108_inpatient"
 (
   encounter_id BIGINT
 , patient_id VARCHAR(255)
+, disease_id BIGINT
 , diagnosis VARCHAR(255)
 , gender VARCHAR(6)
 , birth_date TIMESTAMP
 , admission_date TIMESTAMP
-, time_of_death TIMESTAMP
+, death_date TIMESTAMP
 )
-;
+;;
 
 CREATE TABLE IF NOT EXISTS reporting.eafya_mappings
 (
