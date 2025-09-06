17c7c22813d09553418ffdcbec01b362fdd7a486
pssemanda <ssemandpaul@gmail.com>
2025-09-06 13:25:45 +0300

    added hmis code and report month to preterm labour and birth

--- backend/sql/materializedviews/105_02_maternity_mothers_preterm_labour.sql
+++ backend/sql/materializedviews/105_02_maternity_mothers_preterm_labour.sql
@@ -1,8 +1,10 @@
-create materialized view reporting."105_02_maternity_mothers_preterm_labour" as
-SELECT COUNT(DISTINCT d.patient_id) as total_mothers_preterm_labour
+CREATE MATERIALIZED VIEW reporting."105_02_maternity_mothers_preterm_labour" AS
+SELECT 
+    TO_CHAR(h.admission_date, 'YYYYMM') AS report_month,
+    'MA06' AS hmis_code,
+    COUNT(DISTINCT d.patient_id) as total_mothers_preterm_labour
 FROM reporting.patient_admissions h 
 INNER JOIN reporting.patient_diagnosis d ON h.encounter_id = d.encounter_id 
-WHERE d.disease_name ILIKE '%preterm labour%';
-
-
-
+WHERE d.disease_name ILIKE '%preterm labour%'
+GROUP BY TO_CHAR(h.admission_date, 'YYYYMM')
+ORDER BY report_month;
--- backend/sql/materializedviews/105_02_maternity_preterm_births_in_unit.sql
+++ backend/sql/materializedviews/105_02_maternity_preterm_births_in_unit.sql
@@ -1,8 +1,13 @@
-create materialized view reporting."105_02_maternity_preterm_births_in_unit" as
-SELECT COUNT(DISTINCT d.patient_id) as total_preterm_births_in_unit
+CREATE MATERIALIZED VIEW reporting."105_02_maternity_preterm_births_in_unit" AS
+SELECT 
+    TO_CHAR(h.admission_date, 'YYYYMM') AS report_month,
+    'MA07' AS hmis_code,
+    COUNT(DISTINCT d.patient_id) as total_preterm_births_in_unit
 FROM reporting.patient_admissions h 
 INNER JOIN reporting.patient_diagnosis d ON h.encounter_id = d.encounter_id 
-WHERE d.disease_name ILIKE '%preterm labour%';
+WHERE d.disease_name ILIKE '%preterm labour%'
+GROUP BY TO_CHAR(h.admission_date, 'YYYYMM')
+ORDER BY report_month;
 
 
 
