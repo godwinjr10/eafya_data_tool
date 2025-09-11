b97b3f87ade59ae1b26ba81df44cdf8dd8889b42
Lukwago Joel Jr <contecfy@gmail.com>
2025-09-11 09:44:08 +0300

    test

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -53,7 +53,8 @@ const Sidebar = ({ selected, onSelect, dataSetId }) => {
     <div
       style={{
         minWidth: "200px",
-        background: "#e6f2e6",
+        // background: "#e6f2e6",
+        background: "red",
         padding: 10,
         height: "calc(100vh - 20px)", // This will make it full height minus padding
         overflowY: "auto", // This enables vertical scrolling
